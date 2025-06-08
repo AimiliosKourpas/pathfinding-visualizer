2dfac0e9e8f8adae5c937bbbf5630a877cbb329f
Aimilios Kourpas <aimiliosk2002@gmail.com>
2025-06-08 17:32:42 +0300

    Here we first create a new React component for the pathfinding visualizer, and then we commit the changes to the repository. then we add the necessary files to the staging area and commit them with a message.

---
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,20 @@
+import react, {Component} from "react";
+import Node from "./Node/Node";
+
+import "./PathfindingVisualizer.css";
+
+export default class PathfindingVisualizer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+            Fooo
+            <Node></Node>
+            </div>
+        );  
+    }
+}

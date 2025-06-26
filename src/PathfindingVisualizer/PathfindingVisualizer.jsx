0e6c189b960752dd021b2619159f26dfc8f72add
Aimilios Kourpas <aimiliosk2002@gmail.com>
2025-06-26 20:23:39 +0300

    last check

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -100,15 +100,6 @@ export default class PathfindingVisualizer extends Component {
     }
   }
 
-  animateShortestPath(nodesInShortestPathOrder) {
-    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
-      setTimeout(() => {
-        const node = nodesInShortestPathOrder[i];
-        document.getElementById(`node-${node.row}-${node.col}`).className =
-          'node node-shortest-path';
-      }, 50 * i);
-    }
-  }
 
   visualizeAlgorithm = () => {
     const {
